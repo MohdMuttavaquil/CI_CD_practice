7709f95e1cc3d749ab300b699afaaaca2a03868c
MohdMuttavaquil <mmuttavaquil@gmail.com>
2025-08-27 19:44:30 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Home/Card'
+
+function App() {
+
+  return (
+    <>
+     this is hte practice of CI CD pipeline and imporve skills to leavel up coading.
+     <Card />
+    </>
+  )
+}
+
+export default App
---
+++ src/Home/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='box'>
+      <h1>name:</h1>
+      <p>class</p>
+      <p>Phone number</p>
+      <p>Addres</p>
+    </div>
+  )
+}
+
+export default Card
